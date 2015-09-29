99caae2d450c14b14eecea87cbfffff4d2bdaba5
alan <arzapersonal@gmail.com>
2015-09-29 14:04:55 -0300

    primer commit

---
+++ resources/views/base.blade.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>@yield('title')</title>
+
+  <style type="text/css">
+      body {
+        min-height: 250px;
+        padding-top: 70px;
+      }
+    </style>
+
+  <!-- Bootstrap -->
+  {!! Html::style('assets/css/bootstrap.min.css') !!}
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+  
+</head>
+
+<body>
+
+  <div class="container">
+  <div class="row">
+
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Ubook</a>
+
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+             
+          </ul>
+
+          <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Iniciar Sesion <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+               <fieldset>
+
+                <div class="form-group">
+
+                  <form action="control.php" method="post">
+                    
+                    <div class="form-group">
+                      <div class="col-lg-12">
+                        <input type="text" class="form-control input-sm" id="user" name="user" placeholder="Usuario" required>
+                      </div>
+                    </div>
+
+                    <div class="form-group" >
+                      <div class="col-lg-12" style="margin-top: 5px;">
+                        <input type="password" class="form-control input-sm" id="pass" name="pass" placeholder="Contraseña" required>
+                      </div>
+                    </div>
+
+                    <input id="action" type="hidden" name="action" value="login"/>
+
+                    <div class="col-sm-12" align="center"  style="margin-top: 10px;">         
+                      <button type="submit" class="btn btn-success btn-sm">Ingresar</button>
+                    </div>
+
+                  </form>
+                </div>
+              </fieldset>
+              </ul>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="jumbotron">
+      <h1>Bienvenido!</h1>
+      <p>Bienvenido a Ubook</p>
+    </div>
+
+  </div>
+  </div>
+
+      {!! Html::script('assets/js/jquery-2.1.4.min.js') !!}
+      {!! Html::script('assets/js/bootstrap.min.js') !!}
+
+</body>
+</html>
